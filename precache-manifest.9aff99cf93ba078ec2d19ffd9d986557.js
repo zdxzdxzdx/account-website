20de16f1bfa12afce7beb129b4a7c0d46009d4f7
zdxzdxzdx <769371987@qq.com>
2021-02-21 16:58:52 +0800

    deploy

---
+++ precache-manifest.9aff99cf93ba078ec2d19ffd9d986557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0f8a9bc56d30f59e58",
+    "url": "/account_website/css/app.570c7099.css"
+  },
+  {
+    "revision": "6bbcdb3a0cfd702b04936d3a7820f88f",
+    "url": "/account_website/index.html"
+  },
+  {
+    "revision": "2f0f8a9bc56d30f59e58",
+    "url": "/account_website/js/app.09537007.js"
+  },
+  {
+    "revision": "a0ed12d332cdfde123e7",
+    "url": "/account_website/js/chunk-vendors.5627e520.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account_website/manifest.json"
+  },
+  {
+    "revision": "bc73fe64e0725fa66151378af4d4d9c2",
+    "url": "/account_website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account_website/robots.txt"
+  }
+]);
